37ac0be56a6a1dd5da1361b952dbadcaea49af67
Haider Malik <haidermalik5044@gmail.com>
2016-06-24 01:58:04 +0500

    Added Views with handlebars syntax

---
+++ routes/users.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
+});
+
+router.get('/create', function (req, res, next) {
+    var vm = {title: 'Create and Account'}
+    res.render('users/create', vm);
+
+});
+
+module.exports = router;
